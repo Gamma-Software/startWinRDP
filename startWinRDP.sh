708d4c54782c0182ad1208c538950d07c40f51a0
Rudloff <valentin.rudloff@navya.tech>
2022-01-08 15:21:40 +0100

    add client_ip file and start remmina until pc is pinged

--- startWinRDP.sh
+++ startWinRDP.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mac_address=$(cat mac_address) # This is a hidden file containing the mac address like 0F:0F:0F:0F:0F
-wakeonlan -i 192.168.1.255 -p 7 $mac_address
-sleep 10
-remmina -i -c .local/share/remmina/1641648855466.remmina
+client_ip=$(cat client_ip)
+wakeonlan -i 192.168.1.255 -p 7 $mac_address > /dev/null
+while true; do echo "connecting to distant machine" ; ping -c1 $client_ip > /dev/null && break; done
+remmina -i -c .local/share/remmina/1641648855466.remmina > /dev/null
